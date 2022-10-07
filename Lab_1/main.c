926b7ff4f49d3289d15e3c41f733cd1b5ac2f96d
Ekaterina <ekaterina.cornejo@usach.cl>
2022-10-07 20:37:26 -0300

    ADD: paralelización en algunas funciones

--- Lab_1/main.c
+++ Lab_1/main.c
@@ -74,28 +74,14 @@ int main(int argc, char *argv[])
           HAUX[i*N+j]=0;
         }
       }
-      
-    }          
+    }        
   }
-/*
-  for (int i = 0; i < N; i++)
-  {
-    printf("\n");
-    for (int j = 0; j < N; j++)
-      printf(" %.2f ",H1[i*N+j]);   
-  }
-  printf("\n");
-  */
+
 /////////////////////////////////////////////////////
 //  FIN:   LLenado inicial de la matriz
 ///////////////////////////////////////////////////// 
 
 
-/*
-H1 = t
-H2 = t-2
-HAUX = t-1
-*/
 /////////////////////////////////////////////////////
 //  INICIO:   Calculo de ecuacion
 ///////////////////////////////////////////////////// 
@@ -106,151 +92,68 @@ float iMenos1,iMas1,jMenos1,jMas1;
 int contador = 1;
 int id_imagen = 1;
 
-while( contador <= T)
+#pragma omp parallel private(iMenos1,iMas1,jMenos1,jMas1) shared(contador, id_imagen,c,dt,dd,H1,H2,HAUX,N,T,f,t) num_threads(H)
 {
-
-  //Copia lo que esta en H1 en HAUX
-  swap(H1,HAUX,N);
-  //caso inicial
-  if( contador ==1)
+  printf("Hilos %d", omp_get_num_threads());
+  while( contador <= T)
   {
-    //printf("Se calcula el caso base: %d \n",contador);
-
-    for (int i = 1; i < N-1; i++)
+    //Copia lo que esta en H1 en HAUX
+    swap(H1,HAUX,N);
+    //caso inicial
+    if( contador ==1)
     {
-      for (int j = 1; j < N-1; j++)
+      for (int i = 1; i < N-1; i++)
       {
-        //printf("Pos[%d, %d] \n",i,j);
-        iMenos1=HAUX[(i-1)*N+j];
-        jMenos1=HAUX[i*N+(j-1)];
-        iMas1=HAUX[(i+1)*N+j];
-        jMas1=HAUX[i*N+(j+1)];
+        for (int j = 1; j < N-1; j++)
+        {
+          //printf("Pos[%d, %d] \n",i,j);
+          iMenos1=HAUX[(i-1)*N+j];
+          jMenos1=HAUX[i*N+(j-1)];
+          iMas1=HAUX[(i+1)*N+j];
+          jMas1=HAUX[i*N+(j+1)];
 
-        H1[i*N+j]= HAUX[i*N+j]+ (c*c)*((dt/dd)*(dt/dd))*(iMas1+iMenos1+jMenos1+jMas1-4*HAUX[i*N+j]);
-      }
-      
-    }  
-  }
-  else //caso normal
-  {
-
-    //printf("Se calcula el caso normal: %d \n",contador);
-
-    for (int i = 1; i < N-1; i++)
-    {
-      for (int j = 1; j < N-1; j++)
-      {
-        iMenos1=HAUX[(i-1)*N+j];
-        jMenos1=HAUX[i*N+(j-1)];
-        iMas1=HAUX[(i+1)*N+j];
-        jMas1=HAUX[i*N+(j+1)];
-
-        H1[i*N+j]= 2*HAUX[i*N+j]-H2[i*N+j] +(c*c)*((dt/dd)*(dt/dd))*(iMas1+iMenos1+jMenos1+jMas1-4*HAUX[i*N+j]);
-      }
-      
+          H1[i*N+j]= HAUX[i*N+j]+ (c*c)*((dt/dd)*(dt/dd))*(iMas1+iMenos1+jMenos1+jMas1-4*HAUX[i*N+j]);
+        }
+        
+      }  
     }
-
-  }
-  //Copia lo que esta en HAUX en H2
-  swap(HAUX,H2,N);
-  //Se genera un documento (SECCION CRITICA)
-  if(contador%t==0)
-  {
-    printf(" Se imprime la numero %d \n",contador);
-    /*for (int i = 0; i < N; i++)
+    else //caso normal
+    {
+      for (int i = 1; i < N-1; i++)
       {
-        printf("\n");
-        for (int j = 0; j < N; j++)
-          printf(" %.2f ",H1[i*N+j]);   
+        for (int j = 1; j < N-1; j++)
+        {
+          iMenos1=HAUX[(i-1)*N+j];
+          jMenos1=HAUX[i*N+(j-1)];
+          iMas1=HAUX[(i+1)*N+j];
+          jMas1=HAUX[i*N+(j+1)];
+
+          H1[i*N+j]= 2*HAUX[i*N+j]-H2[i*N+j] +(c*c)*((dt/dd)*(dt/dd))*(iMas1+iMenos1+jMenos1+jMas1-4*HAUX[i*N+j]);
+        }
+        
       }
-      printf("\n\n\n\n");
-    */
-    //printf("\nSE GENERA NUEVA IMAGEN");
-    id_imagen++;
+
+    }
+    //Copia lo que esta en HAUX en H2
+    swap(HAUX,H2,N);
+    //Se genera un documento (SECCION CRITICA)
+    #pragma omp critical
+      if(contador%t==0)
+      {
+        printf(" Se imprime la número %d \n",contador);
+        id_imagen++;
+      }
+      contador++;
+      
   }
-  contador++;
-
 }
-
 /////////////////////////////////////////////////////
 //  FIN:   Calculo de ecuacion
 ///////////////////////////////////////////////////// 
 
   
 
-  //Parte paralalea
-  int *A = (int *) malloc(LARGO*sizeof(int));
 
-  for (int i = 0; i < LARGO; i++)
-  {
-    A[i] = (rand() % (6666 - 0 +1))+0;
-  }
-
-  int max = -1;
-  for (int i = 0; i < LARGO; i++)
-  {
-    if(A[i]>max){
-      max = A[i];
-      //printf("El maximo cambio a %d\n",max);
-    }
-
-  }
-
-  //printf("El mazimo paarlelo es: %d \n",max);
-  int gmax = -1;
-  #pragma omp parallel shared(gmax) num_threads(2)
-  {
-    int maxi = -1;
-
-    #pragma omp for schedule(static, 2)
-      for (int i = 0; i < N; i++)
-      {
-        if(A[i] >maxi)
-        {
-          //printf("El maximo cambio de %d a %d\n",maxi,A[i]);
-          maxi = A[i];
-          
-        }
-      }
-
-    #pragma omp critical
-      if (maxi > gmax)
-        gmax=max;
-
-  }
-
-
-
-
-
-
-
-  //Creacion de prueba de archivo
-
-/*
-  printf("El tamaño de N es: %d",N);
-  saveImg(HImpresion,H1,N);
-
-
-  for (int i = 0; i < N; i++)
-      {
-        printf("\n");
-        for (int j = 0; j < N; j++)
-          printf(" %.2f ",H1[i*N+j]);   
-      }
-      printf("\n\n\n\n");
-
-  printf("El tamaño de N es: %d",N);
-  N=N-2;
-  for (int i = 0; i < N; i++)
-  {
-    printf("\n");
-    for (int j = 0; j < N; j++)
-      printf(" %.2f ",HImpresion[i*N+j]);   
-  }
-  printf("\n");
-
-  */
   printf("\nEl tamaño de N es: %d",N);
   FILE *f1 = fopen("ejemplo.raw", "w");
   fwrite(H1, sizeof(float), N*N, f1);
@@ -262,8 +165,6 @@ while( contador <= T)
   free(HAUX);
   free(HImpresion);
 
-
-  printf("\nEl maximo con omp es: %d\n",gmax);
 
   printf("\nValores de T=%d, t=%d\n",T,t);
 
